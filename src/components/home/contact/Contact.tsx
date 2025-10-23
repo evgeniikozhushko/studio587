37fa4c41f5e7ad3877273b0b248cc6a919375e6a
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2025-10-23 11:58:14 -0600

    Email updated + footer

--- src/components/home/contact/Contact.tsx
+++ src/components/home/contact/Contact.tsx
@@ -5,14 +5,15 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Contact() {
-
   const handleEmailClick = () => {
-    const email = 'hello@studio587.ca';
-    const subject = 'Studio587.ca';
+    const email = "hello@studio587.ca";
+    const subject = "Studio587.ca";
 
-    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}`
-  }
-  
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(
+      subject
+    )}`;
+  };
+
   return (
     <>
       <main className="my-0 space-y-24 md:my-50 md:space-y-48">
@@ -24,20 +25,18 @@ export default function Contact() {
             <div className="text-4xl md:text-5xl font-semibold col-span-1 md:col-span-12">
               587 reasons to work together
             </div>
-            <Button
-              className="mt-2 px-18"
-              onClick={handleEmailClick}
-            >
+            <Button className="mt-2 px-18" onClick={handleEmailClick}>
               Start a project
             </Button>
           </div>
           <div className="col-span-1 md:col-span-2 md:col-start-9 md:row-start-1 flex flex-col gap-1">
             <div className="uppercase text-xs md:text-sm">Contact</div>
-            <div className="text-md font-semibold md:text-lg">hello@studio587.ca</div>
+            <div className="text-md font-semibold md:text-lg">
+              <a href="mailto:hello@studio587.ca">hello@studio587.ca</a>
+            </div>
           </div>
         </section>
       </main>
     </>
   );
 }
-  
--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,25 +4,41 @@ import { Separator } from "@/components/ui/separator";
 
 export default function Footer() {
   return (
-    <footer className="grid w-full grid-cols-1 md:grid-cols-12 px-5 gap-y-4 mt-10 md:mt-20">
+    <footer className="grid w-full grid-cols-1 md:grid-cols-12 px-5 gap-y-4">
       <Separator className="col-span-1 md:col-span-12 my-10" />
 
-      <div className="col-span-1 md:col-span-12">
-        <div className="text-sm uppercase pb-4 text-center">
-          Bridging strategic creativity and advanced technology to build brands
-          and digital platforms that connect audiences and drive growth.
-        </div>
+      <div className="col-span-1 md:col-span-10 mb-10 uppercase">
+        Bridging strategic creativity and advanced technology to build brands
+        and digital platforms that connect audiences and drive growth.
+      </div>
+      <div className="text-xs md:col-span-2">
+        &copy; {new Date().getFullYear()} Studio 587. All rights reserved.
       </div>
 
-      <div className="col-span-1 md:col-span-12">
-        {/* <Separator className="m-10" /> */}
-        <div className="text-[4rem] sm:text-[6rem] md:text-[10rem] lg:text-[14rem] xl:text-[18rem] leading-none font-bold uppercase col-span-1 md:col-span-12 text-center">
-          Studio 587
-        </div>
-        <div className="text-center text-xs m-10">
-          &copy; {new Date().getFullYear()} Studio 587. All rights reserved.
-        </div>
+      <div className="text-[4rem] sm:text-[6rem] md:text-[10rem] lg:text-[14rem] xl:text-[18rem] leading-none font-bold uppercase col-span-1 md:col-span-12 text-center">
+        Studio 587
       </div>
     </footer>
   );
 }
+
+// <footer className="grid w-full grid-cols-1 md:grid-cols-12 px-5 gap-y-4 mt-10 md:mt-20">
+//       <Separator className="col-span-1 md:col-span-12 my-10" />
+
+//       <div className="col-span-1 md:col-span-12">
+//         <div className="text-sm uppercase pb-4 text-center">
+//           Bridging strategic creativity and advanced technology to build brands
+//           and digital platforms that connect audiences and drive growth.
+//         </div>
+//       </div>
+
+//       <div className="col-span-1 md:col-span-12">
+//         {/* <Separator className="m-10" /> */}
+//         <div className="text-[4rem] sm:text-[6rem] md:text-[10rem] lg:text-[14rem] xl:text-[18rem] leading-none font-bold uppercase col-span-1 md:col-span-12 text-center">
+//           Studio 587
+//         </div>
+//         <div className="text-center text-xs m-10">
+//           &copy; {new Date().getFullYear()} Studio 587. All rights reserved.
+//         </div>
+//       </div>
+//     </footer>
