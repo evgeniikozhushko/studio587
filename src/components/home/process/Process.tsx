f1fd536badbd54508422797b83581bc1e64fd85b
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2025-11-01 14:58:45 -0600

    render separators between process items on desktop only, hiding them on mobile.

--- src/components/home/process/Process.tsx
+++ src/components/home/process/Process.tsx
@@ -20,15 +20,16 @@ export default function Process() {
       </section>
 
       <section className="grid w-full grid-cols-1 md:grid-cols-12 px-5 mb-10 md:mb-30 gap-y-4">
-      <Separator className="col-span-1 md:col-span-12 my-14 w-full"/>
-      {/* <div className="col-span-1 md:col-span-12 my-14 border-t border-gray-700 h-[0.25px]" /> */}
+
+      <Separator className="col-span-1 md:col-span-12 my-14"/>
+
       <div className="text-sm uppercase col-span-1 mb-14">What we do</div>
         {processData.map((item: any, index: any) => (
           <div
             key={index}
             className="col-span-1 md:col-span-5 md:col-start-7 mb-8 md:mb-12"
           >
-            {/* { index !==0 && <Separator className="mb-12 w-full"/> } */}
+            { index !==0 && <Separator className="mb-12 w-full hidden mb:block"/> }
            {/* <Separator className="mb-12 w-full"/>  */}
             <div className="flex gap-20">
               <div className="text-sm md:text-sm flex-shrink-0">
