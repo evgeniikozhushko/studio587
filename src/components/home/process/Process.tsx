a8b2e3e2d30775baa21005d137b758138e543f05
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2025-11-01 15:01:12 -0600

    Now the separators between process items will only appear on tablet and desktop views, not on mobile.

--- src/components/home/process/Process.tsx
+++ src/components/home/process/Process.tsx
@@ -29,8 +29,7 @@ export default function Process() {
             key={index}
             className="col-span-1 md:col-span-5 md:col-start-7 mb-8 md:mb-12"
           >
-            { index !==0 && <Separator className="mb-12 w-full hidden mb:block"/> }
-           {/* <Separator className="mb-12 w-full"/>  */}
+            { index !==0 && <Separator className="mb-12 hidden md:block"/> }
             <div className="flex gap-20">
               <div className="text-sm md:text-sm flex-shrink-0">
                 {item.order}
