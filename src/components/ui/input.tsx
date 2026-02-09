8ac736062cc8003b995700acaef7b13e176c0570
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2026-02-09 10:53:46 -0700

    Imnput field text-foreground added

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
   import { cn } from "@/lib/utils"
 
   const inputVariants = cva(
-    "flex w-full rounded-md border bg-background px-3 py-2 text-sm transition-all file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+    "flex w-full rounded-md border bg-background text-foreground px-3 py-2 text-sm transition-all file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
     {
       variants: {
         variant: {
